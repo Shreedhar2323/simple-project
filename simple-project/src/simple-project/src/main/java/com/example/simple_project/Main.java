5f64410524218fe32a9c4ece4dc429d8d9b92cee
Shreedhar2323 <shreedharah2004@gmail.com>
2026-06-22 13:49:53 +0530

    Add files via upload

---
+++ simple-project/src/simple-project/src/main/java/com/example/simple_project/Main.java
@@ -0,0 +1,14 @@
+package com.example.simple_project;
+
+	import com.google.gson.Gson;
+
+	public class Main {
+	    public static void main(String[] args) {
+	      for(int i=0;i<5;i++)
+	      {
+	    	  System.out.print(i);
+	      }
+	    }
+	}
+
+
